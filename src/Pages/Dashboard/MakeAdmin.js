4c3f69071baab9a58639d74a4f667003e78dc272
Sumonche016 <sumonche016@gmail.com>
2022-05-27 04:40:22 +0600

    good night its 4.40am

--- src/Pages/Dashboard/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading/Loading';
 
 const MakeAdmin = () => {
+
+    const { data: users, isLoading } = useQuery('users', () => fetch('http://localhost:5000/users').then(res => res.json()))
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <h1>make admin</h1>
+            <h1>make admin {users.length}</h1>
         </div>
     );
 };
